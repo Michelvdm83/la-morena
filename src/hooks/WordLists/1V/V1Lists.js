11663fc5fdc54afeca555cd6a176711fc901434e
Michelvdm83 <michelvdm@gmail.com>
2025-03-19 11:48:03 +0100

    New Chapters added
    Numbers need testing on Speech page

--- src/hooks/WordLists/1V/V1Lists.js
+++ src/hooks/WordLists/1V/V1Lists.js
@@ -1,3 +1,7 @@
+import gl1V0a from "./1V0a.txt?raw";
+import gl1V0b from "./1V0b.txt?raw";
+import gl1V0c from "./1V0c.txt?raw";
+import gl1V0d from "./1V0d.txt?raw";
 import gl1V1a from "./1V1a.txt?raw";
 import gl1V1b from "./1V1b.txt?raw";
 import gl1V1e from "./1V1e.txt?raw";
@@ -6,9 +10,21 @@ import gl1V2a from "./1V2a.txt?raw";
 import gl1V2b from "./1V2b.txt?raw";
 import gl1V2e from "./1V2e.txt?raw";
 import gl1V2f from "./1V2f.txt?raw";
+import gl1V3a from "./1V3a.txt?raw";
+import gl1V3b from "./1V3b.txt?raw";
+import gl1V3e from "./1V3e.txt?raw";
+import gl1V3f from "./1V3f.txt?raw";
+import gl1V5a from "./1V5a.txt?raw";
+import gl1V5b from "./1V5b.txt?raw";
+import gl1V5e from "./1V5e.txt?raw";
+import gl1V5f from "./1V5f.txt?raw";
 
 export function V1Lists() {
     return [
+        { list: "0 a", item: gl1V0a },
+        { list: "0 b", item: gl1V0b },
+        { list: "0 c", item: gl1V0c },
+        { list: "0 d", item: gl1V0d },
         { list: "1 a", item: gl1V1a },
         { list: "1 b", item: gl1V1b },
         { list: "1 e", item: gl1V1e },
@@ -17,5 +33,13 @@ export function V1Lists() {
         { list: "2 b", item: gl1V2b },
         { list: "2 e", item: gl1V2e },
         { list: "2 f", item: gl1V2f },
+        { list: "3 a", item: gl1V3a },
+        { list: "3 b", item: gl1V3b },
+        { list: "3 e", item: gl1V3e },
+        { list: "3 f", item: gl1V3f },
+        { list: "5 a", item: gl1V5a },
+        { list: "5 b", item: gl1V5b },
+        { list: "5 e", item: gl1V5e },
+        { list: "5 f", item: gl1V5f },
     ];
 }
